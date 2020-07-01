bf4f1aa53f033ff693b45b2a9db40d350db65397
Anatole <anatolededecker@gmail.com>
2020-07-01 15:48:50 +0200

    Quicksort

---
+++ corrections/tris/quick_sort_correction.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <utility>
+#include <cstdlib>
+
+int* partition(int* p_begin, int* p_end) {
+  int* pivot = p_begin + (p_end-p_begin)/2; //Change to random
+  std::swap(*pivot,*(p_end-1));
+  pivot = p_end - 1;
+  int* it1 = p_begin;
+  for(int* it2 = p_begin; it2 != p_end; it2++) {
+    if(*it2 <= *pivot) {
+      std::swap(*it1, *it2);
+      it1++;
+    }
+  }
+  return it1-1;
+}
+
+void quicksort(int* p_begin, int* p_end) {
+  if(p_end-p_begin <= 1) return;
+  int* pivot = partition(p_begin, p_end);
+  quicksort(p_begin,pivot);
+  quicksort(pivot+1,p_end);
+}
+
+int main() {
+  int* a = (int*)malloc(100*sizeof(int));
+  for (int i = 0; i < 100; i++) {
+    a[i] = (4269*i)%100;
+    std::cout<<a[i]<<";";
+  }
+  std::cout<<std::endl;
+  quicksort(a,a+100);
+  for (int i = 0; i < 100; i++) {
+    std::cout<<a[i]<<";";
+  }
+  std::cout<<std::endl;
+  free(a);
+}
